9d67bc7d76caa8d9a4dd511fce2ed79bdb73cc1d
Ariel <127093985+ariel3007@users.noreply.github.com>
2024-09-25 20:46:40 +0300

    Initial commit - convert to kotlin

---
+++ src/main/java/me/olios/plugins/effectmaster/EffectMaster.kt
@@ -0,0 +1,10 @@
+package me.olios.plugins.effectmaster
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class EffectMaster : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+}
